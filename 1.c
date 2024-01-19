233d773331e54fefcd73f671fc53d2953a3baa16
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-01-19 14:22:13 +0530

    hello world

---
+++ 1.c
@@ -0,0 +1,18 @@
+/*
+write a program to print hello world
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf(" *HELLO WORLD*");
+    getch();
+    return 0;
+}
+/*
+OUTPUT :
+ *HELLO WORLD*
+--------------------------------
+Process exited after 2.841 seconds with return value 0
+Press any key to continue . . .
+*/
