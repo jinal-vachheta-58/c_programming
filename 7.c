4203b6896cd730785a998404f2267b6092b6272d
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-01-19 14:27:24 +0530

    print first five odd numbers

---
+++ 7.c
@@ -0,0 +1,27 @@
+/*
+write a program to print first five odd numbers
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i =1;
+    while(i<=10)
+    {
+        printf("%d\n",i);
+        i+=2;
+    }
+    getch();
+    return 0;
+}
+/*
+OUTPUT:
+1
+3
+5
+7
+9
+--------------------------------
+Process exited after 2.685 seconds with return value 0
+Press any key to continue . . .
+*/
